a05b6850eedb7e985293f7bce5a105693c7b7373
Andrei-David Nan <nandavid190@gmail.com>
2024-04-22 16:24:09 +0300

    v0.3 - configure typeorm module, setup config module;

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config/dist/config.module';
+import { DatabaseModule } from './database/database.module';
+import { Module } from '@nestjs/common';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
